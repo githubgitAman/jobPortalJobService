fb9f53e62780cc416fcded56393758fa3c5cabcd
githubgitAman <sharma.aman.a30@gmail.com>
2025-04-22 17:29:11 +0530

    Created Job Service

---
+++ src/test/java/dev/aman/jobportaljobservice/JobPortalJobServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aman.jobportaljobservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalJobServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
